d293a90de89bce521578c5fdd0aaa9f9ce3cdaca
David Young <dave@vipindoorstorage.com>
2023-07-11 03:35:18 -0500

    Expose constants as named interface.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export * from "./service/service.js";
 export * from "./service/ProbeMatch.js"
 export * from "./service/ProbeBuilder.js"
-export * from "./service/device_types.js"
+export * as DeviceTypes from "./service/device_types.js"
